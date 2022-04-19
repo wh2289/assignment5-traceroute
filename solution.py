385d55ea0c3e747cd291d7dffeb41eab3f23b65b
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-04-18 21:22:20 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -57,7 +57,7 @@ def get_route(hostname):
     for ttl in range(1,MAX_HOPS):
         for tries in range(TRIES):
             destAddr = gethostbyname(hostname)
-            icmp = socket.getprotobyname("icmp")
+            icmp = getprotobyname("icmp")
             mySocket = socket(AF_INET,SOCK_RAW, icmp)
 
             mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
