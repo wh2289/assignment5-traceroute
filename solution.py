977c6ff05221ac183810c2897488c1b78256430b
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-04-21 21:01:12 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -100,7 +100,7 @@ def get_route(hostname):
                     rtt = str(round(timeSent * 1000)) + "ms"
                     tracelist1.append([str(ttl), rtt, str(addr[0]), sourceHostname])
                     tracelist2.append(tracelist1[-1])
-                    print(tracelist2)
+                    return tracelist2
 
                 elif types == 3:
                     bytes = struct.calcsize("d")
@@ -108,14 +108,14 @@ def get_route(hostname):
                     rtt = "*"
                     tracelist1.append([str(ttl), rtt, "Request timed out"])
                     tracelist2.append(tracelist1[-1])
-                    print(tracelist2)
+                    return tracelist2
                 elif types == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     rtt = str(round(t - timeSent) * 1000) + "ms"
                     tracelist1.append([str(ttl), rtt, str(addr[0]), sourceHostname])
                     tracelist2.append(tracelist1[-1])
-                    print(tracelist2)
+                    return tracelist2
                 else:
                     tracelist1.append([ttl, "*", "Error"])
                 break
