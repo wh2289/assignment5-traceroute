b68e6c5cb2932d5969cfca423832af0eb89910fd
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-04-22 19:44:08 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -122,11 +122,7 @@ def get_route(hostname):
                 break
             finally:
                 mySocket.close()
-
-
-
-
-
+        return tracelist2
 
 if __name__ == '__main__':
     get_route("google.co.il")
