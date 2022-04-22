e911dbe7716bff530baff98736a4be23e2e204cf
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-04-21 21:06:30 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -97,23 +97,23 @@ def get_route(hostname):
                 if types == 11:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    delay = 0
-                    tracelist1.append([str(ttl), delay, str(addr[0]), sourceHostname])
+                    rtt = str(round(timeSent * 1000)) + "ms"
+                    tracelist1.append([str(ttl), rtt, str(addr[0]), sourceHostname])
                     tracelist2.append(tracelist1[-1])
                     return tracelist2
 
                 elif types == 3:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    delay = 0
-                    tracelist1.append([str(ttl), delay, "Request timed out"])
+                    rtt = "*"
+                    tracelist1.append([str(ttl), rtt, "Request timed out"])
                     tracelist2.append(tracelist1[-1])
                     return tracelist2
                 elif types == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    delay = 0
-                    tracelist1.append([str(ttl), delay, str(addr[0]), sourceHostname])
+                    rtt = str(round(t - timeSent) * 1000) + "ms"
+                    tracelist1.append([str(ttl), rtt, str(addr[0]), sourceHostname])
                     tracelist2.append(tracelist1[-1])
                     return tracelist2
                 else:
